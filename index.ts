040d595462e8d109952673c06180490add5be3c8
Fernando Matos <prudente.matos@hotmail.com>
2019-08-09 23:45:21 -0300

    adicionando projeto

---
+++ index.ts
@@ -0,0 +1,78 @@
+const Busboy = require('busboy');
+import * as fs from 'fs'
+import * as path from 'path'
+import * as os from 'os'
+
+export default (req: any, res: Response, next: any) => {
+    // See https://cloud.google.com/functions/docs/writing/http#multipart_data
+    const busboy = new Busboy({
+      headers: req.headers,
+      limits: {
+        // Cloud functions impose this restriction anyway
+        fileSize: 10 * 1024 * 1024,
+      }
+    });
+  
+    const fields: any = {};
+    const files: any[] = [];
+    const fileWrites: any[] = [];
+    // Note: os.tmpdir() points to an in-memory file system on GCF
+    // Thus, any files in it must fit in the instance's memory.
+    const tmpdir = os.tmpdir();
+  
+    busboy.on('field', (key: any, value: any) => {
+      // You could do additional deserialization logic here, values will just be
+      // strings
+      fields[key] = value;
+    });
+  
+    busboy.on('file', (fieldname: any, file: any, filename: any, encoding: any, mimetype: any) => {
+      const filepath = path.join(tmpdir, filename);
+      console.log(`Handling file upload field ${fieldname}: ${filename} (${filepath})`);
+      const writeStream = fs.createWriteStream(filepath);
+      file.pipe(writeStream);
+  
+      fileWrites.push(new Promise((resolve, reject) => {
+        file.on('end', () => writeStream.end());
+        writeStream.on('finish', () => {
+          fs.readFile(filepath, (err, buffer) => {
+            const size = Buffer.byteLength(buffer);
+            console.log(`${filename} is ${size} bytes`);
+            if (err) {
+              reject(err);
+            }
+  
+            files.push({
+              fieldname,
+              originalname: filename,
+              encoding,
+              mimetype,
+              buffer,
+              size,
+            });
+  
+            try {
+              fs.unlinkSync(filepath);
+            } catch (error) {
+              reject(error);
+            }
+  
+            resolve();
+          });
+        });
+        writeStream.on('error', reject);
+      }));
+    });
+  
+    busboy.on('finish', () => {
+      Promise.all(fileWrites)
+        .then(() => {
+          req.body = fields;
+          req.files = files;
+          next();
+        })
+        .catch(next);
+    });
+  
+    busboy.end(req.rawBody);
+  }
